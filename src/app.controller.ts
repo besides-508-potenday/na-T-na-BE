d2576427dc6c84f8ebc7c7f8f88c9933634f3829
eunkang-choi <dmsrkd1216@gmail.com>
2025-07-22 01:31:47 +0900

    Chore: health-check 초기셋팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,78 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  HealthIndicatorResult,
+  MemoryHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private health: HealthCheckService,
+    private memory: MemoryHealthIndicator,
+    private disk: DiskHealthIndicator,
+    private prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // @Get('simple')
+  @Get('health')
+  @HealthCheck()
+  simpleCheck() {
+    return { status: 'OK', timestamp: new Date().toISOString() };
+  }
+
+  // @Get('health')
+  // @HealthCheck()
+  // async check(): Promise<HealthCheckResult> {
+  //   return this.health.check([
+  //     // 데이터베이스 헬스 체크
+  //     () => this.prismaHealthCheck(),
+  //     // 메모리 사용량 체크 (512MB 이하)
+  //     () => this.memory.checkHeap('memory_heap', 512 * 1024 * 1024),
+  //     // 디스크 사용량 체크(80% 이하)
+  //     () =>
+  //       this.disk.checkStorage('storage', {
+  //         path: '/',
+  //         thresholdPercent: 0.8,
+  //       }),
+  //   ]);
+  // }
+
+  // private async prismaHealthCheck(): Promise<HealthIndicatorResult> {
+  //   try {
+  //     const startTime = Date.now();
+
+  //     // DB연결 응답 시간 측정
+  //     await this.prismaService.$queryRaw`SELECT 1`;
+  //     const responseTime = Date.now() - startTime;
+
+  //     return {
+  //       database: {
+  //         status: 'up',
+  //         message: 'Database connection is healthy',
+  //         responseTime: `${responseTime}ms`,
+  //         timestamp: new Date().toISOString(),
+  //       },
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       database: {
+  //         status: 'down',
+  //         message: `Database connection failed: ${error}`,
+  //         timestamp: new Date().toISOString(),
+  //       },
+  //     };
+  //   }
+  // }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, AuthModule],
+  imports: [UsersModule, CommonModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
