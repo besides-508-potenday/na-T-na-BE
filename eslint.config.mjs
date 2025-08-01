de025f985b38d9470443c1cdecf92d2d998c501a
eunkang-choi <dmsrkd1216@gmail.com>
2025-08-02 02:46:32 +0900

    Chore: 코드 포맷팅(prettier/eslinter 설정)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,11 +32,13 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-assignment': 'warn',
       '@typescript-eslint/no-unsafe-call': 'warn',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' },
-      ],
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/no-unused-vars-experimental': 'warn',
+      'no-unused-vars': 1,
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
 );
