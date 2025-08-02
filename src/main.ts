428c7b38f5326b3984bb11f320d74782e963679a
eunkang-choi <dmsrkd1216@gmail.com>
2025-08-02 18:52:57 +0900

    🔧 Config: 운영배포 자동배포 워크플로우 생성 및 테스트워크플로우 작성

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Rest API 설정
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  await app.listen(3000);
 }
 bootstrap();
