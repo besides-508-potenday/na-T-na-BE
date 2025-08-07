30530490dabde0ed9586595fde9c4f242001ffbb
eunkang-choi <dmsrkd1216@gmail.com>
2025-08-08 00:21:32 +0900

    🔨 Update: 챗봇 래포지토리 인터페이스정의

--- src/chatbots/domain/chatbot.repository.interface.ts
+++ src/chatbots/domain/chatbot.repository.interface.ts
@@ -3,4 +3,7 @@ import ChatbotWithPersonalities from '../infrastructure/chatbot-with-personaliti
 export interface IChatbotRepository {
   // 챗봇 리스트 조회
   getChatbots(): Promise<ChatbotWithPersonalities[]>;
+
+  // 챗봇 1개 정보 조회
+  getChatbotById(id: number);
 }
