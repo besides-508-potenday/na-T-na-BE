40401835c80bc52c9f626a1d5728c0c9f0adaf60
eunkang-choi <dmsrkd1216@gmail.com>
2025-08-08 05:25:23 +0900

    ✨ Feat: 메시지/챗봇소켓 관련 비즈니스규칙 정의

---
+++ src/chatrooms/domain/chatting-socket-business-rule.ts
@@ -0,0 +1,2 @@
+export const OK: string = 'OK';
+export const POLICY_ERROR: string = 'POLICY_ERROR_OCCURRED';
---
+++ src/messages/domain/message-business-rule.ts
@@ -0,0 +1,12 @@
+export enum MessageSenderType {
+  BOT = 'BOT',
+  USER = 'USER',
+}
+
+export const MAXIMUM_MESSAGE_LENGTH = 60;
+
+export const INITIAL_GREETING_FIXED_MESSAGE = (userNickname: string) =>
+  `저기.. ${userNickname}..! 잘지냈어? 혹시 내가 할말이 있는데 들어줄래?`;
+
+export const LAST_FIXED_MESSAGE = (userNickname: string) =>
+  `오늘 너랑 얘기해서 정말 즐거웠어. ${userNickname}! 저기... 사실 너에게 하고 싶은 말이 있어서... 편지로 써봤는데, 혹시 받아줄래? 👉👈`;
