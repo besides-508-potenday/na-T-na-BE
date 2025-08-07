d0498b77883988697b377817e9ab723b9b935210
eunkang-choi <dmsrkd1216@gmail.com>
2025-08-08 05:25:56 +0900

    🔥 Remove: 메시지 비즈니스룰로 이관하여 삭제

--- src/messages/domain/initial-greeting-fixed-message.ts
+++
@@ -1,4 +0,0 @@
-const INITIAL_GREETING_FIXED_MESSAGE = (userNickname: string) =>
-  `저기.. ${userNickname}..! 잘지냈어? 혹시 내가 할말이 있는데 들어줄래?`;
-
-export default INITIAL_GREETING_FIXED_MESSAGE;
--- src/messages/domain/last-fixed-message.ts
+++
@@ -1,3 +0,0 @@
-const LAST_FIXED_MESSAGE = (userNickname: string) =>
-  `오늘 너랑 얘기해서 정말 즐거웠어. ${userNickname}! 저기... 사실 너에게 하고 싶은 말이 있어서... 편지로 써봤는데, 혹시 받아줄래? 👉👈`;
-export default LAST_FIXED_MESSAGE;
